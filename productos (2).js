447fb9fa06bad385fd969106ba2871bfe05035b7
yaelsescovich-hub <yaelsescovich@fourtms.com>
2026-07-13 01:04:23 -0400

    Add files via upload

---
+++ productos (2).js
@@ -0,0 +1,38 @@
+window.PRODUCTOS = [
+  {
+    "nombre": "Owala 475 ml",
+    "categoria": "Otros",
+    "precio": 15000,
+    "precioAntes": 19990,
+    "estado": "Excelente",
+    "medidas": "",
+    "desc": "Color azul. Poco uso",
+    "foto": "img/001.jpg",
+    "estadoVenta": "disponible",
+    "linkPago": ""
+  },
+  {
+    "nombre": "Glisodin 60 capsulas",
+    "categoria": "Otros",
+    "precio": 49990,
+    "precioAntes": 59990,
+    "estado": "Nuevo",
+    "medidas": "",
+    "desc": "",
+    "foto": "img/002.jpg",
+    "estadoVenta": "disponible",
+    "linkPago": ""
+  },
+  {
+    "nombre": "Omega 84 gramos",
+    "categoria": "Otros",
+    "precio": 22000,
+    "precioAntes": 35990,
+    "estado": "Nuevo",
+    "medidas": "",
+    "desc": "",
+    "foto": "img/003.jpg",
+    "estadoVenta": "disponible",
+    "linkPago": ""
+  }
+];
