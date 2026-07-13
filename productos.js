cff9e85d23ac73a450d607972a54e584593c0afd
yaelsescovich-hub <yaelsescovich@fourtms.com>
2026-07-13 13:23:16 -0400

    Add files via upload

---
+++ productos.js
@@ -0,0 +1,14 @@
+window.PRODUCTOS = [
+  {
+    "nombre": "Polera negra",
+    "categoria": "Ropa adulto",
+    "precio": 19990,
+    "precioAntes": 35000,
+    "estado": "Como nuevo",
+    "medidas": "Talla M",
+    "desc": "Color negro. Poco uso",
+    "foto": "img/001.jpg",
+    "estadoVenta": "disponible",
+    "linkPago": ""
+  }
+];
